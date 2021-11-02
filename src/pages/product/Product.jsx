fabb9d41240466baf8a51ac8697c5b02037d83e9
Jesus Alexis <ingjesusalexis@gmail.com>
2021-11-02 14:41:40 -0400

    added products

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,9 +1,12 @@
+import { Add, Remove } from '@material-ui/icons';
 import React from 'react'
 import Anuncios from '../../components/anuncios/Anuncios';
 import Footer from '../../components/footer/Footer';
 import Navbar from '../../components/navbar/Navbar';
 import Newsletter from '../../components/newsletter/Newsletter';
-import {Container, Wrapper, ImgContainer, InfoContainer, Title, Description, Price, Image} from './style';
+import {Container, Wrapper, ImgContainer, InfoContainer, Title, Description, Price, Image, FilterContainer, Filter, FilterColor, FilterTitle, FilterSize, FilterSizeOption, AddContainer, AmountContainer,
+    Amount,
+    Button } from './style';
 
 export const Product = () => {
     return (
@@ -18,6 +21,32 @@ export const Product = () => {
                     <Title>Denin Jumpusut</Title>
                     <Description> Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus odio impedit voluptatum iure perspiciatis, officia magni iste. Odio iure quia, ipsa explicabo accusamus illo iusto repellendus, vitae non aut obcaecati. </Description>
                     <Price>$ 20</Price>
+                    <FilterContainer>
+                        <Filter>
+                            <FilterTitle>Color</FilterTitle>
+                            <FilterColor color="black" />
+                            <FilterColor color="darkblue" />
+                            <FilterColor color ="gray" />
+                        </Filter>
+                        <Filter>
+                            <FilterTitle>Size</FilterTitle>
+                            <FilterSize>
+                                <FilterSizeOption>XS</FilterSizeOption>
+                                <FilterSizeOption>S</FilterSizeOption>
+                                <FilterSizeOption>M</FilterSizeOption>
+                                <FilterSizeOption>L</FilterSizeOption>
+                                <FilterSizeOption>XL</FilterSizeOption>
+                            </FilterSize>
+                        </Filter>
+                    </FilterContainer>
+                    <AddContainer>
+                        <AmountContainer>
+                            <Remove />
+                            <Amount>1</Amount>
+                            <Add />
+                        </AmountContainer>
+                        <Button>ADD TO CART</Button>
+                    </AddContainer>
                 </InfoContainer>
             </Wrapper>
             <Newsletter />
--- src/pages/product/style.js
+++ src/pages/product/style.js
@@ -30,5 +30,66 @@ export const Price = styled.span`
     font-weight: 100;
     font-size: 30px;
 `;
+
+export const FilterContainer = styled.div`
+    width: 50%;
+    margin: 30px 0;
+    display: flex;
+    justify-content: space-between;
+
+`;
+export const Filter = styled.div`
+    display: flex;
+    align-items: center;
+`;
+export const FilterTitle = styled.span`
+    font-size: 20px;
+    font-weight: 200;
+`;
+export const FilterColor = styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${props=>props.color};
+    margin: 0px 5px;
+    cursor: pointer;
+`;
+export const FilterSize = styled.select`
+    
+`;
+export const FilterSizeOption = styled.option``;
+
+export const AddContainer = styled.div`
+    width: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+export const AmountContainer = styled.div`
+    display: flex;
+    font-weight: 700;
+    align-items: center;
+`;
+export const Amount = styled.span`
+    width: 30px;
+    height: 30px;
+    border-radius: 10px;
+    border: 1px solid teal;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0px 5px;
+`;
+export const Button = styled.button`
+    padding: 15px;
+    border: 1px solid teal;
+    background-color: white;
+    cursor: pointer;
+    font-weight: 500;
+
+    &:hover{
+        background-color: #f8f4f4;
+    }
+`;
     
 
