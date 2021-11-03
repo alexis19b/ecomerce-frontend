5d2662d2ed320d4ae3e8b6d07a3cf3759a1a7c59
Jesus Alexis <ingjesusalexis@gmail.com>
2021-11-03 09:42:20 -0400

    product details

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,29 +1,134 @@
-import React from 'react'
-import Anuncios from '../../components/anuncios/Anuncios';
-import Footer from '../../components/footer/Footer';
-import Navbar from '../../components/navbar/Navbar';
-import { Container, Wrapper, Title, Top, Bottom, TopButton, TopText, TopTexts } from './style';
+import { Add, Remove } from "@material-ui/icons";
+import React from "react";
+import Anuncios from "../../components/anuncios/Anuncios";
+import Footer from "../../components/footer/Footer";
+import Navbar from "../../components/navbar/Navbar";
+import {
+  Container,
+  Wrapper,
+  Title,
+  Top,
+  Bottom,
+  TopButton,
+  TopText,
+  TopTexts,
+  Summary,
+  Info,
+  Product,
+  ProductDetail,
+  ProductColor,
+  ProductName,
+  ProductId,
+  ProductSize,
+  PriceDetail,
+  Image,
+  Details,
+  ProductAmountContainer,
+  ProductAmount,
+  ProductPrice,
+  Hr,
+  SummaryItem,
+  SummaryItemPrice,
+  SummaryTitle,
+  SummaryItemText,
+  Botton,
+} from "./style";
 
 const Cart = () => {
-    return (
-        <Container>
-            <Navbar />
-            <Anuncios />
-            <Wrapper>
-                <Title>Your Bag</Title>
-                <Top>
-                    <TopButton>Continue Shopping </TopButton>
-                    <TopTexts>
-                        <TopText>Shopping Bag(2)</TopText>
-                        <TopText>Your Wishlist(0)</TopText>
-                    </TopTexts>
-                    <TopButton type="filled">Checkout Now </TopButton>
-                </Top>
-                <Bottom>Send</Bottom>
-            </Wrapper>
-            <Footer />
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <Navbar />
+      <Anuncios />
+      <Wrapper>
+        <Title>Your Bag</Title>
+        <Top>
+          <TopButton>Continue Shopping </TopButton>
+          <TopTexts>
+            <TopText>Shopping Bag(2)</TopText>
+            <TopText>Your Wishlist(0)</TopText>
+          </TopTexts>
+          <TopButton type="filled">Checkout Now </TopButton>
+        </Top>
+        <Bottom>
+          <Info>
+            <Product>
+              <ProductDetail>
+                <Image src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A" />
+                <Details>
+                  <ProductName>
+                    <b>Product:</b>JESSIE THUNDER SHOES
+                  </ProductName>
+                  <ProductId>
+                    <b>ID:</b>127497464566
+                  </ProductId>
+                  <ProductColor color="black" />
+                  <ProductSize>
+                    <b>Size:</b> 37.5
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>2</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>$ 30</ProductPrice>
+              </PriceDetail>
+            </Product>
+            <Hr />
+            <Product>
+              <ProductDetail>
+                <Image src="https://i.pinimg.com/originals/2d/af/f8/2daff8e0823e51dd752704a47d5b795c.png" />
+                <Details>
+                  <ProductName>
+                    <b>Product:</b>HAKURA T-SHIRT
+                  </ProductName>
+                  <ProductId>
+                    <b>ID:</b>57471156145
+                  </ProductId>
+                  <ProductColor color="gray" />
+                  <ProductSize>
+                    <b>Size:</b> M
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>1</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>$ 25</ProductPrice>
+              </PriceDetail>
+            </Product>
+
+          </Info>
+          <Summary>
+            <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+            <SummaryItem>
+              <SummaryItemText>Subtotal</SummaryItemText>
+              <SummaryItemPrice>$ 75</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Estimated Shipping</SummaryItemText>
+              <SummaryItemPrice>$ 5.90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Shipping Discount</SummaryItemText>
+              <SummaryItemPrice>$ -5.90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem type="total">
+              <SummaryItemText>Total</SummaryItemText>
+              <SummaryItemPrice>$ 80</SummaryItemPrice>
+            </SummaryItem>
+            <Botton>CHECKOUT NOW</Botton>
+          </Summary>
+        </Bottom>
+      </Wrapper>
+      <Footer />
+    </Container>
+  );
+};
 
 export default Cart;
